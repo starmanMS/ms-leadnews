9c4f833a8e3231af6b422cee7ab9432ac9d721fa
Jeff Chen <935865811@qq.com>
2023-09-12 19:08:43 +0800

    集成redis 添加任务业务代码

---
+++ heima-leadnews-service/heima-leadnews-schedule/src/main/java/com/heima/schedule/service/TaskService.java
@@ -0,0 +1,17 @@
+package com.heima.schedule.service;
+
+import com.heima.model.schedule.dtos.Task;
+
+/**
+ * 对外访问接口
+ */
+public interface TaskService {
+
+    /**
+     * 添加任务
+     * @param task   任务对象
+     * @return       任务id
+     */
+    public long addTask(Task task) ;
+
+}
